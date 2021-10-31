1956cd5f6079bdd82294274f7be62c36805ce67b
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2021-10-31 20:01:59 +0100

    Changed blue to green search results

--- scripts/extensions/search.sh
+++ scripts/extensions/search.sh
@@ -3,5 +3,5 @@
 TYPELIST="scripts/extensions/hashtypes"
 
 read -p "Enter search query: " QUERY
-echo -e "\e[34m" && grep -i $QUERY $TYPELIST | sort -h
+echo -e "\e[32m" && grep -i $QUERY $TYPELIST | sort -h
 echo -e "\e[0m"
