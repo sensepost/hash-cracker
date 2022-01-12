012db346d3f4918b130cb59b61ae70f5b3d21075
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2022-01-12 09:39:54 +0100

    Added fingerprint.sh processor

---
+++ scripts/processors/fingerprint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright crypt0rr
+
+# Requirements
+source scripts/selectors/hashtype.sh
+source scripts/selectors/hashlist.sh
+
+# Logic
+$HASHCAT -O --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST --show > tmp_output && cat tmp_output | cut -d ':' -f2 | sort -u | tee tmp_pwonly &>/dev/null; rm tmp_output
+./scripts/extensions/hashcat-utils/bin/expander.bin < tmp_pwonly | sort -u > tmp_expanded && rm tmp_pwonly
+$HASHCAT -O --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST -a 1 tmp_expanded tmp_expanded
+rm tmp_expanded
+echo -e "\n\e[32mFingerprint attack done\e[0m\n"
