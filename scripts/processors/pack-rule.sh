12247f42c9b3d16e594d95663a1053f01f30c2bb
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2022-01-12 10:23:24 +0100

    Renamed pack.sh > pack-rule.sh

--- scripts/processors/pack-rule.sh
+++ scripts/processors/pack-rule.sh
@@ -7,11 +7,11 @@ source scripts/selectors/hashlist.sh
 
 # Logic
 $HASHCAT -O --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST --show | cut -d ':' -f2 | tee tmp_pwonly &>/dev/null
-python2 scripts/extensions/rulegen.py tmp_pwonly
+python2 scripts/extensions/pack/rulegen.py tmp_pwonly
 rm analysis-sorted.word analysis.word analysis-sorted.rule
 
 source scripts/selectors/wordlist.sh
 
 $HASHCAT -O --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST $WORDLIST -r analysis.rule --loopback
 rm analysis.rule tmp_pwonly
-echo -e "\n\e[32mPACK processing done\e[0m\n"
+echo -e "\n\e[32mPACK rule processing done\e[0m\n"
