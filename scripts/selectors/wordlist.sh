139494a72a6f2e0b6a91bf047569f7ccd60a2197
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2022-01-27 12:48:05 +0100

    Extended wordlist selector for option 15

--- scripts/selectors/wordlist.sh
+++ scripts/selectors/wordlist.sh
@@ -2,16 +2,28 @@
 # Copyright crypt0rr
 RESTART="source scripts/selectors/wordlist.sh"
 
-read -e -p "Enter full path to wordlist: " WORDLIST
-if [ -f "$WORDLIST" ]; then
-    echo -e "\e[32mWordlist" $WORDLIST "selected.\e[0m"
-else
-    echo -e "\e[31mFile does not exist, try again.\e[0m"; $RESTART
+### If multiple checks needed use; if ! [[ $START = '15' || '1' ]]; for example
+
+if ! [[ $START = '15' ]]; then
+    read -e -p "Enter full path to wordlist: " WORDLIST
+    if [ -f "$WORDLIST" ]; then
+        echo -e "\e[32mWordlist" $WORDLIST "selected.\e[0m"
+    else
+        echo -e "\e[31mFile does not exist, try again.\e[0m"; $RESTART
+    fi
 fi
 if [[ $START = '8' ]]; then
     read -e -p "Enter full path to second wordlist: " WORDLIST2
     if [ -f "$WORDLIST" ]; then
     echo "\e[32mWordlist" $WORDLIST "selected.\e[0m"
+    else
+        echo -e "\e[31mFile does not exist, try again.\e[0m"; $RESTART
+    fi
+fi
+if [[ $START = '15' ]]; then
+    read -e -p "Enter path to directory containing multiple wordlists: " WORDLIST
+    if [ "$(ls -A $WORDLIST)" ]; then
+    echo -e "\e[32mDirectory" $WORDLIST "selected.\e[0m"
     else
         echo -e "\e[31mFile does not exist, try again.\e[0m"; $RESTART
     fi
