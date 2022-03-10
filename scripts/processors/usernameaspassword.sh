32d34e77593d1ddb7670809b3e20453695acf49c
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2022-03-10 13:27:12 +0100

    Filled the ruleset

--- scripts/processors/usernameaspassword.sh
+++ scripts/processors/usernameaspassword.sh
@@ -7,7 +7,7 @@ source scripts/selectors/hashlist.sh
 
 # Rules
 source scripts/rules/rules.config
-RULELIST=($big)
+RULELIST=($big $d3ad0ne $d3adhob0 $digits1 $digits2 $digits3 $dive $fordyv1 $generated2 $generated3 $hob064 $huge $leetspeak $NSAKEYv2 $ORTRTA $OUTD $pantag $passwordpro $rockyou30000 $techtrip2 $tenKrules $toggles1 $toggles2 $toprules2020 $TOXIC10k $TOXICSP $williamsuper)
 
 # Logic
 cat $HASHLIST | cut -d ':' -f1 | cut -d '\' -f2 > tmp_usernames
