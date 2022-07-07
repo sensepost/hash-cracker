e324e2a47a9d3f6069883b9af2bfd4b3369b9973
Bart <57799908+crypt0rr@users.noreply.github.com>
2022-07-07 13:12:50 +0200

    v2.9.1 (#13)
    * Replaced cut for awk
    
    * Updated version
    
    * Updated README
    
    * Updated VERSION

--- scripts/processors/fingerprint.sh
+++ scripts/processors/fingerprint.sh
@@ -6,7 +6,7 @@ source scripts/selectors/hashtype.sh
 source scripts/selectors/hashlist.sh
 
 # Logic
-$HASHCAT -O --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST --show > tmp_output && cat tmp_output | cut -d ':' -f2 | sort -u | tee tmp_pwonly &>/dev/null; rm tmp_output
+$HASHCAT -O --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST --show > tmp_output && cat tmp_output | awk -F: '{print $NF}' | sort -u | tee tmp_pwonly &>/dev/null; rm tmp_output
 ./scripts/extensions/hashcat-utils/bin/expander.bin < tmp_pwonly | sort -u > tmp_expanded && rm tmp_pwonly
 $HASHCAT -O --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST -a 1 tmp_expanded tmp_expanded
 rm tmp_expanded
--- scripts/processors/prefixsuffix.sh
+++ scripts/processors/prefixsuffix.sh
@@ -7,7 +7,7 @@ source scripts/selectors/hashlist.sh
 
 # Logic
 $HASHCAT -O --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST --show > tmp_prefixsuffix
-cat tmp_prefixsuffix | cut -d ':' -f2 | sort | tee tmp_passwords &>/dev/null && ./scripts/extensions/common-substr -n -p -f tmp_passwords > tmp_prefix && ./scripts/extensions/common-substr -n -s -f tmp_passwords > tmp_suffix && rm tmp_passwords tmp_prefixsuffix
+cat tmp_prefixsuffix | awk -F: '{print $NF}' | sort | tee tmp_passwords &>/dev/null && ./scripts/extensions/common-substr -n -p -f tmp_passwords > tmp_prefix && ./scripts/extensions/common-substr -n -s -f tmp_passwords > tmp_suffix && rm tmp_passwords tmp_prefixsuffix
 $HASHCAT -O --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST -a1 tmp_prefix tmp_suffix
 $HASHCAT -O --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST -a1 tmp_suffix tmp_prefix
 rm tmp_prefix tmp_suffix; echo -e "\nPrefix suffix processing done\n"
