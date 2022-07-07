920cf90d0c504a03a685e43fce6e7bcc8a057785
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2022-07-07 12:59:07 +0200

    Replaced cut for awk

--- scripts/processors/commonsubstring.sh
+++ scripts/processors/commonsubstring.sh
@@ -7,6 +7,6 @@ source scripts/selectors/hashlist.sh
 
 # Logic
 $HASHCAT -O --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST --show > tmp_substring
-cat tmp_substring | cut -d ':' -f2 | sort | tee tmp_passwords &>/dev/null && ./scripts/extensions/common-substr -n -f tmp_passwords > tmp_allsubstrings && rm tmp_passwords tmp_substring
+cat tmp_substring | awk -F: '{print $NF}' | sort | tee tmp_passwords &>/dev/null && ./scripts/extensions/common-substr -n -f tmp_passwords > tmp_allsubstrings && rm tmp_passwords tmp_substring
 $HASHCAT -O --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST -a1 tmp_allsubstrings tmp_allsubstrings
 rm tmp_allsubstrings; echo -e "\nSubstring processing done\n"; main
--- scripts/processors/usernameaspassword.sh
+++ scripts/processors/usernameaspassword.sh
@@ -10,7 +10,7 @@ source scripts/rules/rules.config
 RULELIST=($big $d3ad0ne $d3adhob0 $digits1 $digits2 $digits3 $dive $fordyv1 $generated2 $generated3 $hob064 $huge $leetspeak $NSAKEYv2 $ORTRTA $OUTD $pantag $passwordpro $rockyou30000 $techtrip2 $tenKrules $toggles1 $toggles2 $toprules2020 $TOXIC10k $TOXICSP $williamsuper)
 
 # Logic
-cat $HASHLIST | cut -d ':' -f1 | cut -d '\' -f2 > tmp_usernames
+cat $HASHLIST | awk -F: '{print $NF}' | cut -d '\' -f2 > tmp_usernames
 $HASHCAT -O --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST tmp_usernames
 for RULE in ${RULELIST[*]}; do
     $HASHCAT -O --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST tmp_usernames -r $RULE --loopback
