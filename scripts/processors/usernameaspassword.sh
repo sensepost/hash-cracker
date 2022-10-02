18c44859d1c35429263528d2f2db75b3d36a38f9
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2022-10-02 19:49:56 +0200

    Added new rule to usernamepassword processor

--- scripts/processors/usernameaspassword.sh
+++ scripts/processors/usernameaspassword.sh
@@ -7,7 +7,7 @@ source scripts/selectors/hashlist.sh
 
 # Rules
 source scripts/rules/rules.config
-RULELIST=($big $d3ad0ne $d3adhob0 $digits1 $digits2 $digits3 $dive $fordyv1 $generated2 $generated3 $hob064 $huge $leetspeak $NSAKEYv2 $ORTRTA $OUTD $pantag $passwordpro $rockyou30000 $techtrip2 $tenKrules $toggles1 $toggles2 $toprules2020 $TOXIC10k $TOXICSP $williamsuper)
+RULELIST=($big $fbfull $d3ad0ne $d3adhob0 $digits1 $digits2 $digits3 $dive $fordyv1 $generated2 $generated3 $hob064 $huge $leetspeak $NSAKEYv2 $ORTRTA $OUTD $pantag $passwordpro $rockyou30000 $techtrip2 $tenKrules $toggles1 $toggles2 $toprules2020 $TOXIC10k $TOXICSP $williamsuper)
 
 # Logic
 cat $HASHLIST | cut -d '\' -f2 | awk -F: '{print $1}' > tmp_usernames
