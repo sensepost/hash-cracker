6847b5621bc48705236e0ad976847592719737bf
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2023-01-30 09:59:05 +0100

    Typo

--- scripts/processors/cewl.sh
+++ scripts/processors/cewl.sh
@@ -14,11 +14,11 @@ done
 read -p "Minimum word length: " LENGTH
 until [[ $LENGTH = [0-9] ]]; do
      echo "Please only use 0-9."
-     read -p "Depth to spider to (0-9): " LENGTH
+     read -p "Minimum word length (0-9): " LENGTH
 done
 
 echo -e "\nCeWL is going to start, this will take some time..."
-echo -e "\nNOTE: If it takes to long, use CTRL+C to stop where CeWL is currently at, this will result in an output file."
+echo -e "\nNOTE: If it takes to long, use CTRL+C to stop where CeWL is currently at, this will result in an output file.\n"
 
 $CEWL -d $DEPTH -m $LENGTH -w $CEWLLIST $URL -u "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
 
