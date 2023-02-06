a2c37330b7afe9350e5796c8bfbfe70ade23dd79
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2023-02-06 13:05:05 +0100

    Added rule processing to digitremover

--- scripts/processors/digitremover.sh
+++ scripts/processors/digitremover.sh
@@ -5,6 +5,11 @@
 source scripts/selectors/hashtype.sh
 source scripts/selectors/hashlist.sh
 
+# Rules
+source scripts/rules/rules.config
+RULELIST=($fbfull $ORTRTS $NSAKEYv2 $techtrip2 $generated3)
+
+# Digitfilter
 cat $POTFILE | awk -F: '{print $NF}' | sed 's/[0-9]//g' | tee tmp_digitfiltered &>/dev/null
 
 # Logic
@@ -14,5 +19,9 @@ $HASHCAT $KERNEL --bitmap-max=24 --hwmon-disable --potfile-path=$POTFILE -m$HASH
 $HASHCAT $KERNEL --bitmap-max=24 --hwmon-disable --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST -a6 tmp_digitfiltered '?s?d?d?d?d' --increment
 $HASHCAT $KERNEL --bitmap-max=24 --hwmon-disable --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST -a6 tmp_digitfiltered '?d?d?d?d?s' --increment
 $HASHCAT $KERNEL --bitmap-max=24 --hwmon-disable --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST -a6 tmp_digitfiltered '?a?a' --increment
+
+for RULE in ${RULELIST[*]}; do
+    $HASHCAT $KERNEL --bitmap-max=24 --hwmon-disable --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST tmp_digitfiltered -r $RULE --loopback
+done
 rm tmp_digitfiltered
 echo -e "\nDigit removal / Hybrid processing done\n"
