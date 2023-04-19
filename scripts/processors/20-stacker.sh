a1daf66d3d2311d177975cab0568fd3c9618e77d
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2023-04-19 20:19:07 +0200

    Added new processor 20-stacker.sh

---
+++ scripts/processors/20-stacker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright crypt0rr
+
+# Requirements
+source scripts/selectors/hashtype.sh
+source scripts/selectors/hashlist.sh
+
+# Single or multiple wordlist
+read -p "Single or Multiple wordlist mode? S/M: " MODE
+
+if [[ $MODE = [Ss] ]]; then
+    source scripts/selectors/wordlist.sh
+elif [[ $MODE = [Mm] ]]; then
+    source scripts/selectors/multiple-wordlist.sh
+fi
+
+# Rules
+source scripts/rules/rules.config
+RULELIST=($rule3 $fbtop $toprules2020 $digits1 $digits2 $hob064 $leetspeak $toggles1 $toggles2 $OUTD)
+
+# Logic
+$HASHCAT $KERNEL --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST $WORDLIST -r $stacking58 -r $stacking58 $LOOPBACK
+for RULE in ${RULELIST[*]}; do
+    $HASHCAT $KERNEL --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST $WORDLIST -r $stacking58 -r $RULE $LOOPBACK
+done
+echo -e "\nStacking with light rules done\n"
