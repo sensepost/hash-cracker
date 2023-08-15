0ed45bcfb28334f3b3db1000964e9a1403577edf
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2023-08-15 14:38:43 -0700

    Wordlists checking based on staticconfig

--- scripts/selectors/wordlist.sh
+++ scripts/selectors/wordlist.sh
@@ -2,9 +2,17 @@
 # Author: crypt0rr - https://github.com/crypt0rr/
 RESTART="source scripts/selectors/wordlist.sh"
 
-read -e -p "Enter full path to wordlist: " WORDLIST
-if [ -f "$WORDLIST" ]; then
-    echo "Wordlist" $WORDLIST "selected."
+if [[ "$STATICCONFIG" = true ]]; then
+    if [ -f "$WORDLIST" ]; then
+        echo "Wordlist" $WORDLIST "selected."
+    else
+        echo "Wordlist 1 does not exist, edit static configuration in 'hash-cracker.conf'."; exit
+    fi
 else
-    echo "File does not exist, try again."; $RESTART
+    read -e -p "Enter full path to wordlist: " WORDLIST
+    if [ -f "$WORDLIST" ]; then
+        echo "Wordlist" $WORDLIST "selected."
+    else
+        echo "File does not exist, try again."; $RESTART
+    fi
 fi
