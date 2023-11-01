38bd43ac621038ea2b32dda3ad166df88e8a32b4
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2023-11-01 14:22:43 +0100

    Expander change

---
+++ scripts/mac.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\nOptional modules:"
+if [[ -x "scripts/extensions/common-substr-mac" ]]; then
+    echo '[+] common-substr-mac is executable'
+else
+    echo '[-] common-substr-mac is not executable or found (option 10 / 11)'
+fi
+if [[ -x "scripts/extensions/hashcat-utils-mac/bin/expander.bin" ]]; then
+    echo '[+] Expander is executable'
+else
+    echo '[-] Expander is not available/executable or found, this is needed for fingerprint cracking'
+fi
+if [[ -x "scripts/extensions/cewl/cewl.rb" ]]; then
+    echo '[+] CeWL is executable'
+    CEWL="scripts/extensions/cewl/cewl.rb"
+else
+    echo '[-] CeWL is not executable or found (option 18)'
+fi
